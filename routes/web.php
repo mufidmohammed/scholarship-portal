e8944c5a9c7e94faf1c9e78b4deae1fa9189409d
mufidmohammed <mufidmohammed200@gmail.com>
2023-05-27 22:15:54 +0000

    added admin site

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Livewire\Upload;
 use App\Http\Livewire\Summary;
 use App\Http\Livewire\Education;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ReviewController;
 use App\Http\Livewire\GuardianInformation;
 use App\Http\Livewire\PersonalInformation;
@@ -22,6 +23,10 @@ use App\Http\Controllers\ProfileController;
 |
 */
 
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
 Route::get('/', function () {
     return redirect()->route('login');
 });;
@@ -32,7 +37,11 @@ Route::middleware(['auth'])->group(function () {
         if (auth()->user()->type == 'reviewer') {
             return redirect()->route('review.applicants');
         }
-        return redirect()->route('personal');
+        else if (auth()->user()->type == 'applicant') {
+            return redirect()->route('personal');
+        } else {
+            return to_route('admin.index');
+        }
     })->name('dashboard');
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -49,20 +58,24 @@ Route::middleware(['auth', 'is_applicant'])->group(function () {
     Route::get('/summary', Summary::class)->name('summary');
 });
 
-Route::middleware(['auth', 'is_reviewer'])->group(function () {
-    Route::get('/review-applicants', [ReviewController::class, 'index'])->name('review.applicants');
-    Route::get('/review-applicant/{id}', [ReviewController::class, 'show'])->name('review.detail');
-    Route::put('/review-grant/{id}', [ReviewController::class, 'grant'])->name('review.grant');
-    Route::put('/review-dismiss/{id}', [ReviewController::class, 'dismiss'])->name('review.dismiss');
+Route::middleware(['auth', 'is_reviewer'])->prefix('review')->group(function () {
+    Route::get('applicants', [ReviewController::class, 'index'])->name('review.applicants');
+    Route::get('applicant/{id}', [ReviewController::class, 'show'])->name('review.detail');
+    Route::put('grant/{id}', [ReviewController::class, 'grant'])->name('review.grant');
+    Route::put('dismiss/{id}', [ReviewController::class, 'dismiss'])->name('review.dismiss');
 
-    Route::get('/review-granted', function() {
+    Route::get('granted', function() {
         return view('review.granted');
     })->name('review.granted');
 
-    Route::get('/review-dismissed', function() {
+    Route::get('dismissed', function() {
         return view('review.dismissed');
     })->name('review.dismissed');
 
+});
+
+Route::middleware(['auth', 'is_admin'])->group(function () {
+    Route::resource('admin', AdminController::class)->except('show');
 });
 
 require __DIR__.'/auth.php';
